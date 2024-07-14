5a84c6791cf514db8b100017cbbe5d3fd13e3a10
Lenin Khangjrakpam <leninkhangjrakpam@gmail.com>
2024-07-14 20:51:01 +0530

    fix config.go error

--- go.mod
+++ go.mod
@@ -7,15 +7,14 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
-	github.com/joho/godotenv v1.5.1
-	golang.org/x/crypto v0.24.0
-	github.com/redis/go-redis/v9 v9.5.3
+	github.com/redis/go-redis/v9 v9.5.4
 	golang.org/x/oauth2 v0.21.0
 )
 
 require (
-	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	cloud.google.com/go/compute/metadata v0.5.0 // indirect
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	golang.org/x/sys v0.22.0 // indirect
 )
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,20 +1,20 @@
-Advances in Neural Information Processing Systems. Vol. 35. Curran Associatespackage config
+// Advances in Neural Information Processing Systems. Vol. 35. Curran Associates
+package config
 
 type ContextKeyClaims string
 type ContextKeyCredentials string
 
 const (
-	APIversion string = "/api/v1"
-	OAuthCookieName string = "oauth_credentials"
-	JWTAuthCookieName string = "Authorization"
-	JWTRefreshTokenCookieName  string = "RefreshToken"
+	APIversion                string = "/api/v1"
+	OAuthCookieName           string = "oauth_credentials"
+	JWTAuthCookieName         string = "Authorization"
+	JWTRefreshTokenCookieName string = "RefreshToken"
 
-	ClaimsContextKey ContextKeyClaims = "claims"
+	ClaimsContextKey      ContextKeyClaims      = "claims"
 	CredentialsContextKey ContextKeyCredentials = "credentials"
 
-	AdminRole string = "admin"
-	UserRole string = "user"
-	BcryptHashingCost int = 12 // it will do 2^12 rounds of hashing
-	RegexEmail string = `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`
+	AdminRole         string = "admin"
+	UserRole          string = "user"
+	BcryptHashingCost int    = 12 // it will do 2^12 rounds of hashing
+	RegexEmail        string = `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`
 )
-
